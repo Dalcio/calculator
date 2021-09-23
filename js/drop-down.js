1f2fd816e74899c14a46d2a448646f082fd90ca2
Dálcio Garcia <dalciomacuetegarcia@gmail.com>
2021-09-23 14:22:24 +0100

    feat: update drop down

--- js/drop-down.js
+++ js/drop-down.js
@@ -1,5 +1,4 @@
 const drop_down_btn = document.querySelector(".dropdown-select > .drop-btn");
-const drop_items = document.querySelector(".dropdown-select > .drop-items");
 
 drop_down_btn.addEventListener("click", function ({ target }) {
   const active = target.getAttribute("active") === "true";
@@ -7,26 +6,28 @@ drop_down_btn.addEventListener("click", function ({ target }) {
 });
 
 document.addEventListener("click", ({ target }) => {
-  if (
-    target.hasAttribute("selected") &&
-    [...target.classList].includes("option")
-  ) {
-    // disabling the current active
-    const current_active = document.querySelector(
-      `.dropdown-select > .options > .option[value="${drop_down_btn.getAttribute(
-        "value"
-      )}"]`
-    );
+  if (drop_down_btn.getAttribute("active") === "true") {
+    if (
+      target.hasAttribute("selected") &&
+      [...target.classList].includes("option")
+    ) {
+      // disabling the current active
+      const current_active = document.querySelector(
+        `.dropdown-select > .options > .option[value="${drop_down_btn.getAttribute(
+          "value"
+        )}"]`
+      );
 
-    // active the current element
-    current_active.setAttribute("selected", "false");
-    drop_down_btn.textContent = target.textContent;
-    const value = target.getAttribute("value");
-    drop_down_btn.setAttribute("value", value);
-    target.setAttribute("selected", "true");
+      // active the current element
+      current_active.setAttribute("selected", "false");
+      drop_down_btn.textContent = target.textContent;
+      const value = target.getAttribute("value");
+      drop_down_btn.setAttribute("value", value);
+      target.setAttribute("selected", "true");
 
-    drop_down_btn.setAttribute("active", "false");
-  } else if (!target.hasAttribute("active") && target.id !== "drop-btn") {
-    drop_down_btn.setAttribute("active", "false");
+      drop_down_btn.setAttribute("active", "false");
+    } else if (!target.hasAttribute("active") && target.id !== "drop-btn") {
+      drop_down_btn.setAttribute("active", "false");
+    }
   }
 });
