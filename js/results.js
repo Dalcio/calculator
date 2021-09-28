e93962dbdc67d8b77fe01185a97734463503b091
Dálcio Garcia <dalciomacuetegarcia@gmail.com>
2021-09-28 15:06:08 +0100

    feat: results ui

---
+++ js/results.js
@@ -0,0 +1,23 @@
+"use strict";
+/* ==== global vars area ==== */
+let results = [];
+/* end global vars area */
+
+// clear the results
+const clear_results = document.querySelector("button.clear");
+
+clear_results.addEventListener("click", () => {
+  const results_container = document.querySelector("div.results");
+
+  results_container.innerHTML = "";
+  results = [];
+});
+
+// add this on function responsible to append the results
+// handle the result when clicked
+// const results_equation = document.querySelector(".results > .equation");
+
+// results_equation.addEventListener("click", () => {
+//   // put me into input field
+//   alert("put me into input field");
+// });
