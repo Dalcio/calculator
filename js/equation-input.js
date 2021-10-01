90cde599a5d2e404138ba074a4528c575069380a
Dálcio Garcia <dalciomacuetegarcia@gmail.com>
2021-10-01 13:28:14 +0100

    feat: focus input when click and after dom load

--- js/equation-input.js
+++ js/equation-input.js
@@ -1,6 +1,9 @@
 /**
  * This function auto focus the equation input on start
- */ (function focusEquationInputOnStart() {
+ */
+function focusEquationInputOnStart() {
   const operation_input = document.querySelector(".equation-input");
   operation_input.focus();
-})();
+}
+document.addEventListener("DOMContentLoaded", focusEquationInputOnStart);
+document.addEventListener("click", focusEquationInputOnStart);
